7cbb7dc9f288b0c9558c09cf657b2caff15247ae
Jssdvdgrcprz <jssdvdgrcprz@gmail.com>
2026-05-25 16:11:40 -0500

    feat: primer commit

---
+++ src/turistupar/Turistupar.java
@@ -0,0 +1,16 @@
+package turistupar;
+
+/**
+ *
+ * @author jssdv
+ */
+public class Turistupar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
